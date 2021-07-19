3d249a3c47475205197c5264d27e5c3cf618bf24
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-19 12:28:01 +0530

    feat: configure env variables

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
+require("dotenv").config();
+const { PORT } = require("./config");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
-const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-})
+const port = PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
 /**
  * Error handling middleware
@@ -14,5 +16,5 @@ app.get('/', (req, res) => {
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log('server listening on port: ', port)
-})
+  console.log("server listening on port: ", port);
+});
