8f9787236c9ac3fa0a290b071f859f0dcdc0f43b
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-19 14:43:32 +0530

    fix: apply eslint style rules

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require("express");
-require("dotenv").config();
-const {
-    PORT
-} = require("./config");
-const {
-    ApplicationError
-} = require("./lib/index");
-const errorHandler = require("./middleware/errorHandler");
+const express = require('express');
+require('dotenv').config();
+const { PORT } = require('./config');
+const { ApplicationError } = require('./lib/index');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 const port = PORT || 3000;
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
 
 /**
@@ -24,14 +20,14 @@ app.use(errorHandler);
  * 404 Error handler
  * Note: DO NOT MOVE. This should be the last route
  */
-app.all("*", (req, res, next) => {
-    const err = new ApplicationError(
-        `can't find ${req.originalUrl} on server`,
-        404
-    );
-    next(err);
+app.all('*', (req, res, next) => {
+  const err = new ApplicationError(
+    `can't find ${req.originalUrl} on server`,
+    404,
+  );
+  next(err);
 });
 
 app.listen(port, () => {
-    console.log("server listening on port: ", port);
+  console.log('server listening on port: ', port);
 });
