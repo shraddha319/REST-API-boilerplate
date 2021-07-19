ee8371ceba74c6e4877d59664817527f865abce2
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-19 12:31:56 +0530

    refactor: import from common lib index file

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 require("dotenv").config();
-const { PORT } = require("./config");
-const { ApplicationError } = require("./lib/api/error");
+const {
+    PORT
+} = require("./config");
+const {
+    ApplicationError
+} = require("./lib/index");
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 const port = PORT || 3000;
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+    res.send("Hello, world!");
 });
 
 /**
@@ -21,13 +25,13 @@ app.use(errorHandler);
  * Note: DO NOT MOVE. This should be the last route
  */
 app.all("*", (req, res, next) => {
-  const err = new ApplicationError(
-    `can't find ${req.originalUrl} on server`,
-    404
-  );
-  next(err);
+    const err = new ApplicationError(
+        `can't find ${req.originalUrl} on server`,
+        404
+    );
+    next(err);
 });
 
 app.listen(port, () => {
-  console.log("server listening on port: ", port);
+    console.log("server listening on port: ", port);
 });
