ce3162ac507f3e879975050b10c6b3c735acf61a
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-20 19:55:43 +0530

    feat: added get, delete handlers for /user/:userId

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
-const { catchAsync } = require('../lib/index');
+const { catchAsync, ApplicationError, deepMerge } = require('../lib/index');
+const { User } = require('../models/user.model');
 const validateUserRegistration = require('../middleware/validateUserRegistration');
+const validateUserId = require('../middleware/validateUserId');
 const { postNewUser } = require('../controller/user.controller');
 
 const router = express.Router();
@@ -9,5 +11,33 @@ const router = express.Router();
 router
   .route('/')
   .post(catchAsync(validateUserRegistration), catchAsync(postNewUser));
+
+router.use('/:userId', catchAsync(validateUserId));
+
+router
+  .route('/:userId')
+  .get((req, res) => {
+    const { user } = req;
+    res.json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  })
+  .delete(
+    catchAsync(async (req, res, next) => {
+      const { user } = req;
+
+      const removedUser = await user.remove();
+      res.json({
+        status: 'success',
+        data: {
+          // eslint-disable-next-line no-underscore-dangle
+          userId: removedUser._id,
+        },
+      });
+    }),
+  );
 
 module.exports = router;
