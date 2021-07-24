957df6eaf5a029820808c0084bb5803bc3eea30a
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-24 22:35:14 +0530

    refactor: use sendResponse

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,13 +1,20 @@
 const { User } = require('../models/user.model');
+const { sendResponse } = require('../lib/index');
 
 const postNewUser = async (req, res, next) => {
   const { user } = req.body;
   const savedUser = await User.create(user);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      user: savedUser,
-    },
+  // res.status(201).json({
+  //   status: 'success',
+  //   data: {
+  //     user: savedUser,
+  //   },
+  // });
+  return sendResponse({
+    res,
+    success: true,
+    payload: { user: savedUser },
+    statusCode: 201,
   });
 };
 
