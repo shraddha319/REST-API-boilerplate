c3f44d658e7c12be2cef976e5787d4818d37ef8f
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-25 20:28:44 +0530

    refactor: seperate module for route handlers

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,15 +1,9 @@
 const { User } = require('../models/user.model');
-const { sendResponse } = require('../lib/index');
+const { sendResponse, deepMerge } = require('../lib/index');
 
 const postNewUser = async (req, res, next) => {
   const { user } = req.body;
   const savedUser = await User.create(user);
-  // res.status(201).json({
-  //   status: 'success',
-  //   data: {
-  //     user: savedUser,
-  //   },
-  // });
   return sendResponse({
     res,
     success: true,
@@ -18,6 +12,41 @@ const postNewUser = async (req, res, next) => {
   });
 };
 
+const getUserById = (req, res) => {
+  const { user } = req;
+  return sendResponse({ res, payload: { user }, success: true });
+};
+
+const updateUserById = async (req, res, next) => {
+  const { update } = req.body;
+  const { user } = req;
+  deepMerge(user, update);
+  await user.save();
+  // res.status(204).json({ status: 'success' });
+  return sendResponse({ res, success: true, statusCode: 204 });
+};
+
+const deleteUserById = async (req, res, next) => {
+  const { user } = req;
+  const removedUser = await user.remove();
+  // res.json({
+  //   status: 'success',
+  //   data: {
+  //     userId: removedUser._id,
+  //   },
+  // });
+  return sendResponse({
+    res,
+    success: true,
+    statusCode: 200,
+    // eslint-disable-next-line no-underscore-dangle
+    payload: { userId: removedUser._id },
+  });
+};
+
 module.exports = {
   postNewUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,43 +1,24 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
-const { catchAsync, ApplicationError, deepMerge } = require('../lib/index');
-const { User } = require('../models/user.model');
-const validateUserRegistration = require('../middleware/validateUserRegistration');
+const { catchAsync } = require('../lib/index');
 const validateUserId = require('../middleware/validateUserId');
-const { postNewUser } = require('../controller/user.controller');
+const {
+  postNewUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+} = require('../controller/user.controller');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .post(catchAsync(validateUserRegistration), catchAsync(postNewUser));
+router.route('/').post(catchAsync(postNewUser));
 
 router.use('/:userId', catchAsync(validateUserId));
 
 router
   .route('/:userId')
-  .get((req, res) => {
-    const { user } = req;
-    res.json({
-      status: 'success',
-      data: {
-        user,
-      },
-    });
-  })
-  .delete(
-    catchAsync(async (req, res, next) => {
-      const { user } = req;
-
-      const removedUser = await user.remove();
-      res.json({
-        status: 'success',
-        data: {
-          // eslint-disable-next-line no-underscore-dangle
-          userId: removedUser._id,
-        },
-      });
-    }),
-  );
+  .get(getUserById)
+  .post(catchAsync(updateUserById))
+  .delete(catchAsync(deleteUserById));
 
 module.exports = router;
