e184bdde3d90a06d68c153baa2453202d3207e00
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-27 17:02:42 +0530

    test: POST /user 400 validation errors

--- routes/user.test.js
+++ routes/user.test.js
@@ -62,4 +62,86 @@ describe('testing /user endpoint', () => {
     expect(user._id).toBeTruthy();
     expect(statusCode).toBe(201);
   });
+
+  test('should not register user if required fields are absent on POST /user', async () => {
+    /**
+     * email, password, first name, dob are required fields
+     * should give VALIDATION_ERROR with status code 400
+     */
+    try {
+      await request(app).post('/user').send({ user: {} });
+    } catch (error) {
+      expect(error.success).toBe(false);
+      expect(error.code).toBe('VALIDATION_ERROR');
+      expect(error.statusCode).toBe(400);
+    }
+    const user = await User.find({});
+    expect(user.length).toBe(0);
+  });
+
+  test('should not register user if email is not unique', async () => {
+    const email = 'shraddha1998@gmail.com';
+    const password = 'shraddha1998';
+    const firstName = 'shraddha98';
+    const DOB = '1998-09-21';
+
+    try {
+      await request(app)
+        .post('/user')
+        .send({ user: { email, password, firstName, DOB } });
+      await request(app)
+        .post('/user')
+        .send({ user: { email, password, firstName, DOB } });
+    } catch (error) {
+      expect(error.success).toBe(false);
+      expect(error.code).toBe('VALIDATION_ERROR');
+      expect(error.message).toBe(`${email} already exists`);
+      expect(error.statusCode).toBe(400);
+    }
+
+    const user = await User.find({});
+    expect(user.length).toBe(1);
+  });
+
+  test('should not register user if not older than 13 years', async () => {
+    const email = 'shraddha1998@gmail.com';
+    const password = 'shraddha1998';
+    const firstName = 'shraddha98';
+    const DOB = '2011-09-21'; // 10 years old
+
+    try {
+      await request(app)
+        .post('/user')
+        .send({ user: { email, password, firstName, DOB } });
+    } catch (error) {
+      expect(error.success).toBe(false);
+      expect(error.code).toBe('VALIDATION_ERROR');
+      expect(error.message).toBe('User must be atleast 13 years of age.');
+      expect(error.statusCode).toBe(400);
+    }
+
+    const user = await User.find({});
+    expect(user.length).toBe(0);
+  });
+
+  test("should not register user if password doesn't meet criteria", async () => {
+    const email = 'shraddha1998@gmail.com';
+    const password = '1998';
+    const firstName = 'shraddha98';
+    const DOB = '2011-09-21'; // 10 years old
+
+    try {
+      await request(app)
+        .post('/user')
+        .send({ user: { email, password, firstName, DOB } });
+    } catch (error) {
+      expect(error.success).toBe(false);
+      expect(error.code).toBe('VALIDATION_ERROR');
+      expect(error.message).toBe('Password must be atleast 8 characters long.');
+      expect(error.statusCode).toBe(400);
+    }
+
+    const user = await User.find({});
+    expect(user.length).toBe(0);
+  });
 });
