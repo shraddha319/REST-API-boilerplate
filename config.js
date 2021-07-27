e8c3b3cec2a4541baba165fce08f2e5ff1d433de
shraddha319 <shraddha.jain3219@gmail.com>
2021-07-27 16:43:11 +0530

    ci: configure DB connection string based on env

--- config.js
+++ config.js
@@ -3,12 +3,15 @@ const {
   DB_NAME,
   DB_USERNAME,
   DB_PASSWORD,
+  DB_TEST,
   NODE_ENV,
   SALT_WORK_FACTOR,
   JWT_SECRET,
 } = process.env;
 
-const DB_CONNECTION_STRING = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.xnwmi.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const DB_CONNECTION_STRING = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.xnwmi.mongodb.net/${
+  NODE_ENV === 'test' ? DB_TEST : DB_NAME
+}?retryWrites=true&w=majority`;
 
 module.exports = {
   PORT,
