8a47e219bc4e55084129307fb3292690e0241e43
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-06 18:47:29 +0530

    rename middleware -> middlewares

--- middlewares/validateUserId.js
+++ middlewares/validateUserId.js
@@ -1,15 +1,16 @@
 const { User } = require('../models/user.model');
-const { ApplicationError, ErrorTypes } = require('../lib/index');
+const { ApplicationError, ErrorTypes } = require('../lib');
 
 const { RESOURCE_NOT_FOUND } = ErrorTypes;
 
 const validateUserId = async (req, res, next) => {
   const { userId } = req.params;
   const user = await User.findById(userId);
+
   if (!user)
     return next(
       new ApplicationError(RESOURCE_NOT_FOUND, {
-        message: `can't find userId: ${userId}`,
+        message: `${userId} not found`,
       }),
     );
   req.userId = userId;
