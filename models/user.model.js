97e16fd6a61ca876cfee2359898d8e9d90a9f11a
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-06 18:50:25 +0530

    fix: remove mongoose validation

--- models/user.model.js
+++ models/user.model.js
@@ -1,54 +1,35 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const {
-  validateDOB,
-  validateUniqueField,
-} = require('../middleware/validateUserRegistration');
 const { SALT_WORK_FACTOR } = require('../config');
 
 const userSchema = mongoose.Schema({
   email: {
     type: String,
-    required: [true, 'Email is a required field.'],
+    required: true,
   },
   password: {
     type: String,
-    required: [true, 'Password is a required field.'],
-    minLength: [8, 'Password must be atleast 8 characters long.'],
+    required: true,
+    minLength: 8,
   },
   firstName: {
     type: String,
-    required: [true, 'First name is a required field.'],
+    required: true,
   },
   lastName: { type: String },
-  DOB: {
-    type: Date,
-    required: [true, 'Date Of Birth is required.'],
-    validate: {
-      validator: validateDOB,
-      message: 'User must be atleast 13 years of age.',
-    },
-  },
 });
 
-/**
- * unique is not a validator. Only indicates it can be used for creating mongodb index
- * email - immutable, unique
- * password - 8 char, alpha + special
- * firstname, lastname - only alphabets
- * DOB - age above 13
- */
+userSchema.statics.isEmailTaken = async function (email, excludeId) {
+  const user = await this.findOne({ email, _id: { $ne: excludeId } });
+  return Boolean(user);
+};
 
-userSchema.path('email').validate({
-  validator: validateUniqueField('email', 'User'),
-  message: '{VALUE} already exists',
-  type: 'unique',
-});
-
-userSchema.post('validate', async (user, next) => {
-  if (!user.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(Number(SALT_WORK_FACTOR));
-  user.password = await bcrypt.hash(user.password, salt);
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    const salt = await bcrypt.genSalt(Number(SALT_WORK_FACTOR));
+    user.password = await bcrypt.hash(user.password, salt);
+  }
   return next();
 });
 
