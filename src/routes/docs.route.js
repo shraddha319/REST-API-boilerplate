b5d8a19f566ae8614d7eaedc40e22efc0df624fd
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-07 15:30:09 +0530

    fix: update import pathname

--- src/routes/docs.route.js
+++ src/routes/docs.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const swaggerOptions = require('../docs/swaggerOptions');
+const swaggerOptions = require('../../docs/swaggerOptions');
 
 // TODO update all docs
 
